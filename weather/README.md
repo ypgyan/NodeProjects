8b26bd71237dfce30ef285d40366b96f30619549
Yan de Paula <yanw100@gmail.com>
2019-03-01 10:22:24 -0300

    Comitando o projeto weather.

---
+++ weather/README.md
@@ -0,0 +1,6 @@
+# Weather
+
+Weather consome a API forecastio, recebendo o ZIP code e retornando a temperatura do local.
+Projeto usa jquery, express, EJS.
+O Weather foi feito apenas como um projeto para estudo e para aplicar os ensinos do livro Express-in-action, podendo
+receber melhorias ao longo do tempo apenas por diversão ^^.
---
+++ weather/app.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var path = require("path");
+var zipdb =  require("zippity-do-dah");
+var ForecastIo = require("forecastio");
+
+var app = express();
+var weather = new ForecastIo("4e815b348d45920374b8d5ef6f27bb73");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res, next) {
+    res.render("index");
+});
+
+app.get(/^\/(\d{5})$/, function(req, res, next) {
+    var zipcode = req.params[0];
+    var location = zipdb.zipcode(zipcode);
+    if (!location.zipcode) {
+        next();
+        return;
+    }
+
+    var latitude = location.latitude;
+    var longitude = location.longitude;
+
+    weather.forecast(latitude, longitude, function(err, data) {
+        if (err) {
+            next();
+            return;
+        }
+        
+        res.json({
+            zipcode: zipcode,
+            temperature: data.currently.temperature
+        })
+    });
+});
+
+app.use(function(req, res) {
+    res.status(404).render("404");
+})
+
+app.listen(3000);
